d78da609d12adf0c03fb5db2290695e0e0bfea89
Rajat <rajat@webkorps.com>
2020-12-24 17:56:54 +0530

    Update

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,11 +2,7 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.user_roles.new(role_id: params[:user][:role_id])
-    if @user.save
-      render success: true
-    else
-      render json: @user.errors.full_messages 
-    end
+    @user.save ? (render success: true) : (render json: @user.errors.full_messages)
   end 
   def update
     @user = User.find_by(email: params[:user][:email])
