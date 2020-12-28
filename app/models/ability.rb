c72ce50bb93fef68048175839370bf9496bf9117
Rajat <rajat@webkorps.com>
2020-12-28 16:58:17 +0530

    check this update

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,9 @@ class Ability
     if user.present?
       user_role = User.find_by(authentication_token: user.authentication_token).roles.pluck:name
       can :manage, :all if user_role.include? "admin"
+      # can :manage, :all if user.roles.pluck(:name).include? "admin"
+      # can [:read], Role if user_role.include? "manager"
+      # can [:read], Role if user_role.include? "employee"
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_token_authenticatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :f_name, presence: true
   validates :l_name, presence: true
-  validates :dob, presence: true
-  validates :doj, presence: true
+  validates :dob,    presence: true
+  validates :doj,    presence: true
   has_many :user_roles
   has_many :roles, through: :user_roles
+  has_many :reviews
   accepts_nested_attributes_for :user_roles, allow_destroy: true
 end
