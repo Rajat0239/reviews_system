64554e16f3ca10460602a7fb8a48f5faa406c28b
Rajat <rajat@webkorps.com>
2020-12-31 11:08:51 +0530

    updated

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,15 +5,15 @@ class Api::UsersController < ApplicationController
     @user.save ? (render json: @user) : (render json: @user.errors.full_messages)
   end 
   def update
-     if ((current_user.roles.pluck(:name).include? "admin") && @user = User.find(params[:id])) 
-      @user.update(params.require(:user).permit(:f_name, :l_name, :dob, :doj, {user_roles_attributes: [:id, :role_id]}))
-      render json: @user.roles
-     elsif current_user.id == params[:id].to_i
-      current_user.update(params.require(:user).permit(:password, :f_name, :l_name, :dob))
-      render json: current_user.roles
-     else
-      render json: "you can not update"
-     end
+    if ((current_user.roles.pluck(:name).include? "admin") && @user = User.find(params[:id])) 
+    @user.update(params.require(:user).permit(:f_name, :l_name, :dob, :doj, {user_roles_attributes: [:id, :role_id]}))
+    render json: @user.roles
+    elsif current_user.id == params[:id].to_i
+    current_user.update(params.require(:user).permit(:password, :f_name, :l_name, :dob))
+    render json: current_user.roles
+    else
+    render json: "you can not update"
+    end
   end
   private
     def user_params
