09cd746e7457f9203abd2a2347ee8768b222887b
Anu <anu@webkorps.com>
2021-01-04 18:12:46 +0530

    Updated review

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,9 +1,15 @@
 class Api::ReviewsController < ApplicationController
   def index
-    (roles.include? "admin") ? (render json: Review.select(:id, :ratings, :feedback).where(status: true)) : (render json: current_user.reviews)
+    (roles.include? "admin") ? (render json: Review.select(:id, :ratings, :feedback).where(status:true)) : (render json: current_user.reviews)
   end
   def create
-    !review_status ? (@review = current_user.reviews.new(review_params); @review.quarter = current_quarter; @review.user_current_role = current_user.current_role; @review.reporting_user_current_role = User.find(params[:reporting_user_id]).current_role; @review.save ? (render json: @review) : (@review.errors.full_messages)) : (render json: "You have given review for this quarter")
+    if !review_status
+      @review = current_user.reviews.new(review_params)
+      @review.reporting_user_current_role = User.find(params[:review][:reporting_user_id]).current_role
+      @review.save ? (render json: @review) : (render json: @review.errors.full_messages)
+    else
+      render json: "You have given review for this quarter"
+    end
   end
   def update
     if current_user.id == Review.find(params[:id]).reporting_user_id
@@ -17,6 +23,6 @@ class Api::ReviewsController < ApplicationController
   end
   private
     def review_params
-      params.permit(:ratings , :feedback, :reporting_user_id)
+      params.require(:review).permit(:ratings , :feedback, :reporting_user_id).merge({quarter: current_quarter, user_current_role: current_user.current_role })
     end
 end
