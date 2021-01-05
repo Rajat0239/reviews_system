22b558518885977bb31b17a6912658ea151b9b2d
Rajat <rajat@webkorps.com>
2021-01-05 18:19:14 +0530

    mail done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ApplicationMailer
   default from: "rajat.gupta290998@gmail.com"
   def sample_email(user)
     #byebug
-    @user = User.find(user.user_id)
-    #@email = "anurajamma01@gmail.com"
-    mail(to: @user.email, subject: 'Your Review Is Not Approved')
+    #@user = User.find(user.user_id)
+    #@email = "anurajamma01@gmail.com"0
+    mail(to: "rajat.gupta.sbg@gmail.com", subject: 'Your Review Is Not Approved')
   end
 end
