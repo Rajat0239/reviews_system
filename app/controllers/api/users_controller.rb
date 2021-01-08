9b2caad6668bb79ded820e2c7371421bafe76cd3
Anu <anu@webkorps.com>
2021-01-08 19:17:10 +0530

    User_changes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,32 +1,39 @@
 class Api::UsersController < ApplicationController
   def create
-    @user = User.new(user_params)
-    @user.user_roles.new(role_id: params[:user][:role_id])
-    @user.current_role = Role.find_role(params[:user][:role_id])
-    @user.save ? (render json: @user) : (render json: @user.errors.full_messages)
+    reporting_role_is_manager ? (
+    @user = User.new(user_params);
+    @user.user_roles.new(role_id: params[:role_id]);
+    @user.current_role = Role.find_role(params[:role_id]);
+    @user.save ? (render json: @user) : (render json: @user.errors.full_messages)) : (render json: "Please select reporting user id of manager")
   end 
 
   def update
-    #byebug
-    # if ((roles.include? "admin") && @user = User.find_user(params[:id])
-    #   @user.current_role = Role.find(params[:user][:user_roles_attributes]['0'][:role_id].to_i).name
-    #   @user.update(params.require(:user).permit(:f_name, :l_name, :dob, :doj, {user_roles_attributes: [:id, :role_id]}))
-    #   render json: @user.roles
-    # elsif current_user.id == params[:id].to_i
-    #   current_user.update(params.require(:user).permit(:password, :f_name, :l_name, :dob))
-    #   render json: current_user.roles
-    # else
-    #   render json: "you can not update"
-    # end
+    if ((roles.include? "admin") && @user = User.find_user(params[:id])) && reporting_role_is_manager
+      @user.current_role = Role.find_role(params[:user_roles_attributes]['0'][:role_id].to_i)
+      (@user.update(update_params)) ? (render json: @user) : (render json: @user.errors.full_messages)
+    elsif current_user.id == params[:id].to_i 
+      current_user.update(params.require(:user).permit(:password, :f_name, :l_name, :dob))
+      render json: current_user.roles
+    else
+      render json: "You can not update"
+    end
   end
 
   def destroy
     @user.destroy
-    render json: "User Destroyed"
+    render json: "User inside the data destroyed"
   end
 
   private
     def user_params
-      params.require(:user).permit(:email, :password, :f_name, :l_name, :dob, :doj , :reporting_user_id) 
+      params.permit(:email, :password, :f_name, :l_name, :dob, :doj , :reporting_user_id) 
+    end
+
+    def update_params
+      params.permit(:f_name, :l_name, :dob, :doj, :reporting_user_id, {user_roles_attributes: [:id, :role_id]})
+    end
+
+    def reporting_role_is_manager
+      return (@user = User.find_user(params[:reporting_user_id]).current_role.include? "manager")
     end
 end
