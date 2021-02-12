588ae89e67687e4bd4e0e0eb94922bfee51b80dc
deepakkushwah <Webkorps@123>
2021-02-12 17:25:18 +0530

    feedback

--- app/controllers/api/feedback_by_reporting_users_controller.rb
+++ app/controllers/api/feedback_by_reporting_users_controller.rb
@@ -1,7 +1,5 @@
 class Api::FeedbackByReportingUsersController < ApplicationController
-
   before_action :check_review_of_user, only: [:create]
-
   def show
     if role_is_admin
       @feedback = FeedbackByReportingUser.where(feedback_for_user_id:params[:feedback_for_user_id],quarter:current_quarter)
@@ -12,35 +10,27 @@ class Api::FeedbackByReportingUsersController < ApplicationController
     end
       (@feedback.empty?) ? (render :json => {:message => "Sorry! feedback is not available"}) : (render json: @feedback.as_json(only: [:id, :user_id, :feedback, :feedback_for_user_id]))
   end
-
+  
   def create
     @error = create_feedbacks(params[:feedback_by_reporting_users])
     unless @error.present?
       UserMailer.reprting_feedback_email(@feedback).deliver_now
-      render json: {message: "Feedback created successfully"}
+      render json: {message: "Feedback created successfully",}
     else
       render json: @error
     end
   end
-
   def update
     @feedback_by_reporting_user = FeedbackByReportingUser.where(feedback_for_user_id: params[:user_id])
     @feedback_by_reporting_user.update(quarter: current_quarter, status: params[:feedback_by_reporting_users][:status]) ? ( render :json => {:message=> "Feedback status update successfully!"}; UserMailer.employee_feedback_acknowledgement_mail(params[:user_id]).deliver_now) : (render json: "can't update this feedback")
   end
-
   private
-
   def create_feedbacks(array_of_data)
     error = ""
     ActiveRecord::Base.transaction do
       array_of_data.map do |data|
         @user = User.find(data[:feedback_for_user_id])
         if @user.reporting_user_id == current_user.id
-          @ratings =  current_user.ratings.new(quarter: current_quarter, ratings_by_user: ratings, reporting_user_id: current_user.reporting_user_id)
-          unless @ratings.save  
-          error = @ratings.errors.full_messages
-          raise ActiveRecord::Rollback
-          end
           @feedback = current_user.feedback_by_reporting_users.new(data.as_json)
           @feedback.quarter = current_quarter
           unless @feedback.save
@@ -54,14 +44,11 @@ class Api::FeedbackByReportingUsersController < ApplicationController
     end
     error
   end
-
-  def date_params
-    params.require(:feedback_by_reporting_users).permit(:review_id, :feedback, :status)
-  end
-
-  def check_review_of_user
-    @review = Review.where(id: params[:feedback_by_reporting_users][0][:review_id], quarter:current_quarter)
-    render json: "Sorry! You con't give feedback for this user becouse this use review not available " if @review.empty?
-  end
-
+    def date_params
+      params.require(:feedback_by_reporting_users).permit(:review_id, :feedback, :status)
+    end
+    def check_review_of_user
+      @review = Review.where(id: params[:feedback_by_reporting_users][0][:review_id], quarter:current_quarter)
+      render json: "Sorry! You con't give feedback for this user becouse this use review not available " if @review.empty?
+    end
 end
