0237e49bbc48cb07f32381e59136d811e01da9f1
simran <simran@webkorps.com>
2021-03-16 18:01:06 +0530

    updated request code

--- app/controllers/inventory_system/asset_requests_controller.rb
+++ app/controllers/inventory_system/asset_requests_controller.rb
@@ -1,5 +1,11 @@
 class InventorySystem::AssetRequestsController < ApplicationController
   def index
-    render json: AssetRequest.where(status: false)
+    render json: AssetRequest.joins(:user).joins(:asset).where(status: false).select("asset_requests.id, users.id as user_id, assets.id as asset_id, users.f_name,users.l_name, assets.name, asset_requests.reason")
+  end
+
+  def update
+    if @asset_request.update(status: true)
+      success_response('request declined')
+    end
   end
 end
